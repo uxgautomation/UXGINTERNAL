99e0677a9b76f10083806e69febc116af50ab00b
uxgautomation@gmail.com <uxgpassword1>
2015-12-16 16:05:56 +0200

    test first version

--- src/test/java/Tests/SimultaneouslySOISubmission.java
+++ src/test/java/Tests/SimultaneouslySOISubmission.java
@@ -1,10 +1,8 @@
 package Tests;
 
 import Common.TestHelper;
-import PageObjects.FrontPage;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SimultaneouslySOISubmission {
@@ -13,12 +11,10 @@ public class SimultaneouslySOISubmission {
     public static String second = "0";
     public static String third = "0";
 
-    @BeforeClass
-    public static void precondition() {
-    }
     @Before
     public void setUp() {
-        TestHelper.droneDriver("chrome");
+        TestHelper.runDriver("chrome");
+        TestHelper.setFullscreen();
         TestHelper.get("https://test2.vocalpoint.com/campaigns/autotest0001/consumer-facing-sample-opt-name/1?page-1");
     }
     @After
@@ -31,30 +27,22 @@ public class SimultaneouslySOISubmission {
         TestHelper.waitXpathElement("//*[@class='wl-unreg-user-menu']/a[1]").click();
         TestHelper.waitXpathElement("//*[@id='edit-name']").sendKeys("20151210112512@mailforspam.com");
         TestHelper.waitXpathElement("//*[@id='edit-pass']").sendKeys("Uxgpassword1");
-        TestHelper.waitXpathElement("//*[@id='edit-submit']").isDisplayed();
-        TestHelper.waitXpathElement("//*[@id='edit-submit']").isEnabled();
         TestHelper.waitXpathElement("//*[@id='edit-submit']").click();
         TestHelper.waitXpathElement("//*[contains(@class,'wl-user-menu')]//*[contains(@class,'user-picture')]");
-        TestHelper.scrollXpathElement("//input[@type='submit']").click();
-        TestHelper.waitXpathElement("//input[contains(@class,'first-name')]").sendKeys("Firstname");
-        TestHelper.waitXpathElement("//input[contains(@class,'last-name')]").sendKeys("Lastname");
-        TestHelper.waitXpathElement("//input[contains(@class,'thoroughfare')]").sendKeys("10 BROADWAY");
-        TestHelper.waitXpathElement("//input[contains(@class,'locality')]").sendKeys("NEW YORK");
-        TestHelper.selectVisibleTextInDropdownXpath("//select[contains(@class,'state')]","New York");
-        TestHelper.waitXpathElement("//input[contains(@class,'postal-code')]").sendKeys("10004-1703");
+        TestHelper.waitXpathElement("//input[@type='submit']").isEnabled();
         TestHelper.waitXpathElement("//input[@type='submit']").click();
-        TestHelper.waitXpathElement("//input[@type='radio']").click();
-        for(int i=0;i<300;i++) {
-            if(TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").isDisplayed()
-                    && TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").isEnabled()) {
+        TestHelper.waitXpathElement("//input[contains(@class,'first-name')]");
+        for(int i=0;i<3000;i++) {
+            if(TestHelper.waitXpathElement("//input[@type='submit']").isDisplayed()
+                    && TestHelper.waitXpathElement("//input[@type='submit']").isEnabled()) {
                 first = "1";
                 break;
             }
             TestHelper.waitMsec(100);
         }
-        for(int i=0;i<300;i++) {
+        for(int i=0;i<3000;i++) {
             if(first.equals("1") && second.equals("1") && third.equals("1") ) {
-                TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").click();
+                TestHelper.waitXpathElement("//input[@type='submit']").click();
                 break;
             }
             TestHelper.waitMsec(100);
@@ -68,30 +56,22 @@ public class SimultaneouslySOISubmission {
         TestHelper.waitXpathElement("//*[@class='wl-unreg-user-menu']/a[1]").click();
         TestHelper.waitXpathElement("//*[@id='edit-name']").sendKeys("20151210130625@mailforspam.com");
         TestHelper.waitXpathElement("//*[@id='edit-pass']").sendKeys("Uxgpassword1");
-        TestHelper.waitXpathElement("//*[@id='edit-submit']").isDisplayed();
-        TestHelper.waitXpathElement("//*[@id='edit-submit']").isEnabled();
         TestHelper.waitXpathElement("//*[@id='edit-submit']").click();
         TestHelper.waitXpathElement("//*[contains(@class,'wl-user-menu')]//*[contains(@class,'user-picture')]");
-        TestHelper.scrollXpathElement("//input[@type='submit']").click();
-        TestHelper.waitXpathElement("//input[contains(@class,'first-name')]").sendKeys("Firstname");
-        TestHelper.waitXpathElement("//input[contains(@class,'last-name')]").sendKeys("Lastname");
-        TestHelper.waitXpathElement("//input[contains(@class,'thoroughfare')]").sendKeys("10 BROADWAY");
-        TestHelper.waitXpathElement("//input[contains(@class,'locality')]").sendKeys("NEW YORK");
-        TestHelper.selectVisibleTextInDropdownXpath("//select[contains(@class,'state')]","New York");
-        TestHelper.waitXpathElement("//input[contains(@class,'postal-code')]").sendKeys("10004-1703");
+        TestHelper.waitXpathElement("//input[@type='submit']").isEnabled();
         TestHelper.waitXpathElement("//input[@type='submit']").click();
-        TestHelper.waitXpathElement("//input[@type='radio']").click();
-        for(int i=0;i<300;i++) {
-            if(TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").isDisplayed()
-                    && TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").isEnabled()) {
+        TestHelper.waitXpathElement("//input[contains(@class,'first-name')]");
+        for(int i=0;i<3000;i++) {
+            if(TestHelper.waitXpathElement("//input[@type='submit']").isDisplayed()
+                    && TestHelper.waitXpathElement("//input[@type='submit']").isEnabled()) {
                 second = "1";
                 break;
             }
             TestHelper.waitMsec(100);
         }
-        for(int i=0;i<300;i++) {
+        for(int i=0;i<3000;i++) {
             if(first.equals("1") && second.equals("1") && third.equals("1") ) {
-                TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").click();
+                TestHelper.waitXpathElement("//input[@type='submit']").click();
                 break;
             }
             TestHelper.waitMsec(100);
@@ -105,30 +85,22 @@ public class SimultaneouslySOISubmission {
         TestHelper.waitXpathElement("//*[@class='wl-unreg-user-menu']/a[1]").click();
         TestHelper.waitXpathElement("//*[@id='edit-name']").sendKeys("20151211105952@mailforspam.com");
         TestHelper.waitXpathElement("//*[@id='edit-pass']").sendKeys("Uxgpassword1");
-        TestHelper.waitXpathElement("//*[@id='edit-submit']").isDisplayed();
-        TestHelper.waitXpathElement("//*[@id='edit-submit']").isEnabled();
         TestHelper.waitXpathElement("//*[@id='edit-submit']").click();
         TestHelper.waitXpathElement("//*[contains(@class,'wl-user-menu')]//*[contains(@class,'user-picture')]");
-        TestHelper.scrollXpathElement("//input[@type='submit']").click();
-        TestHelper.waitXpathElement("//input[contains(@class,'first-name')]").sendKeys("Firstname");
-        TestHelper.waitXpathElement("//input[contains(@class,'last-name')]").sendKeys("Lastname");
-        TestHelper.waitXpathElement("//input[contains(@class,'thoroughfare')]").sendKeys("10 BROADWAY");
-        TestHelper.waitXpathElement("//input[contains(@class,'locality')]").sendKeys("NEW YORK");
-        TestHelper.selectVisibleTextInDropdownXpath("//select[contains(@class,'state')]","New York");
-        TestHelper.waitXpathElement("//input[contains(@class,'postal-code')]").sendKeys("10004-1703");
+        TestHelper.waitXpathElement("//input[@type='submit']").isEnabled();
         TestHelper.waitXpathElement("//input[@type='submit']").click();
-        TestHelper.waitXpathElement("//input[@type='radio']").click();
-        for(int i=0;i<300;i++) {
-            if(TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").isDisplayed()
-                    && TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").isEnabled()) {
+        TestHelper.waitXpathElement("//input[contains(@class,'first-name')]");
+        for(int i=0;i<3000;i++) {
+            if(TestHelper.waitXpathElement("//input[@type='submit']").isDisplayed()
+                    && TestHelper.waitXpathElement("//input[@type='submit']").isEnabled()) {
                 third = "1";
                 break;
             }
             TestHelper.waitMsec(100);
         }
-        for(int i=0;i<300;i++) {
+        for(int i=0;i<3000;i++) {
             if(first.equals("1") && second.equals("1") && third.equals("1") ) {
-                TestHelper.waitXpathElement("//a[contains(@class,'vp_addr_btn_submit')]").click();
+                TestHelper.waitXpathElement("//input[@type='submit']").click();
                 break;
             }
             TestHelper.waitMsec(100);
